4032f9c0b617e17d3a86725fbdb3dd6c978d3dfa
grayolaRSA <gtndanga@gmail.com>
2023-03-09 21:17:39 +0200

    update

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,5 +3,4 @@ from add_0 import add
 if __name__ == "__main__":
     a = 1
     b = 2
-    total = add(a, b)
-    print(str(a) + " + " + str(b) + " = " + str(total))
+    print( "{0} + {1} = {2}".format(a, b, add(a, b)))
