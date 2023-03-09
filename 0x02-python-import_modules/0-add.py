b3b0cdfd171d5b3f39d22fcd8b39671935fa8202
grayolaRSA <gtndanga@gmail.com>
2023-03-09 21:03:16 +0200

    update

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-
 from add_0 import add
-a = 1
-b = 2
-total = a + b
-print(f'{a} + {b} = {total}')
+
+
+def zero_add():
+    a = 1
+    b = 2
+    total = add(a, b)
+    print(f'{a} + {b} = {total}')
+
 
 if __name__ == "__main__":
-    import sys
+    zero_add()
