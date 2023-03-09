64a82280cdf9e07ca5f7849c10d04ae66d4d7412
grayolaRSA <gtndanga@gmail.com>
2023-03-09 22:09:43 +0200

    update

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,7 +4,9 @@ import types
 
 
 if __name__ == "__main__":
-    hidden_4 = imp.load_compiled("hidden_4", "hidden_4.pyc")
+    spec = importlib.util.spec_from_file_location("hidden_4", "hidden_4.pyc")
+    hidden_4 = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(hidden_4)
     names = [name for name in dir(hidden_4) if not name.startswith("__")]
     names.sort()
     for name in names:
