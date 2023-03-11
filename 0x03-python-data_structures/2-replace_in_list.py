6293571921d8e45c9c0eabb3eaa240a64353139a
grayolaRSA <gtndanga@gmail.com>
2023-03-11 19:30:06 +0200

    update

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,8 +3,7 @@
 
 def replace_in_list(my_list, idx, element):
     if idx >= 0 and idx < len(my_list):
-        my_list.pop(idx)
-        my_list.insert(idx - 1, element)
+        my_list[idx] = element
         return my_list
     else:
-        return my_list.copy()
+        return my_list
