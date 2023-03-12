1787b864ced25ec2087f31eb19ad8361a583ce53
grayolaRSA <gtndanga@gmail.com>
2023-03-12 18:23:25 +0200

    update

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    new_list = []
+    for i in range(len(my_list)):
+        if i < 0 or i > len(my_list):
+            return my_list
+        elif i != idx:
+            new_list.append(my_list[i])
+    return new_list
