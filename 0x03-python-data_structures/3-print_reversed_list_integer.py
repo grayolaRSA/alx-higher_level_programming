459a2ac7416ea2b63b51d10c53e88d37618d57e7
grayolaRSA <117780335+grayolaRSA@users.noreply.github.com>
2023-03-13 09:16:13 +0200

    Update 3-print_reversed_list_integer.py
    Shorten code

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
+
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
-    for idx in range(len(my_list)):
-        print("{:d}".format(my_list[idx]))
+    if my_list:
+        for i in reversed(my_list):
+            print("{:d}".format(i))
