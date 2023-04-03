590c1c766bba39a52905c71fea3504d69dfce473
grayolaRSA <gtndanga@gmail.com>
2023-04-03 19:43:22 +0200

    update

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+""" Class that defines a rectangle"""
+
+
+class Rectangle:
+    """defines a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """intialises a rectangle
+        Args:
+        width (int): width of rectangle
+        height (int): height of rectangle
+        """
+
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """
+        Gets value of width(int)
+        """
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        changes the value of the width
+        Args:
+        value(int): new value of width
+        """
+
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__width = value
+
+        @property
+        def height(self):
+            """
+            Gets value of rectangle height (int)
+            """
+
+            return self.__height
+
+        @height.setter
+        def height(self, value):
+            """
+            changes the value of the rectangle height
+            Args:
+            value(int): new value of height
+            """
+
+            if type(value) != int:
+                raise TypeError("width must be an integer")
+            if value < 0:
+                raise ValueError("height must be >= 0")
+            self.__height = value
