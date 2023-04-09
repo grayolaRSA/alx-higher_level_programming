d53bd0d91cff8045f115adc12050b3dfa507996c
grayolaRSA <gtndanga@gmail.com>
2023-04-09 18:28:24 +0200

    update

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -11,4 +11,7 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
-    print('My name is {}{}'.format(first_name.strip(), (' ' + last_name.strip()) if last_name else ''))
+    if last_name:
+        print(f'My name is {first_name} {last_name}')
+    else:
+        print(f'My name is {first_name}')
