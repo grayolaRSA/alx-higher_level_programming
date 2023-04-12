dd505a0fc1dba2edebd29e9666da6055593f4b2c
grayolaRSA <gtndanga@gmail.com>
2023-04-12 20:46:32 +0200

    update

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""module that saves object to file"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    function writes object to file
+
+    Args:
+    my_obj: object
+    filename: file
+
+    Return:
+    Nothing
+    """
+    with open(filename, 'w', encoding='utf-8') as file:
+        json.dumps(my_obj)
