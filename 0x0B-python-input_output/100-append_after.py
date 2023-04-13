8e1f1ee926b2e38553485472b29975bbca347b3a
grayolaRSA <gtndanga@gmail.com>
2023-04-13 21:45:38 +0200

    update

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module for a text file insertion function"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Insert text after each line containing a given string in a file.
+    Args:
+        filename (str): Name of file
+        search_string (str): String to be searched for within the file
+        new_string (str): String to be inserted
+    """
+    string = ""
+    with open(filename) as f:
+        for line in f:
+            string += line
+            if search_string in line:
+                string += new_string
+    with open(filename, "w") as w:
+        w.write(text)
