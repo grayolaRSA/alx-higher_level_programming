e3ec9dba9a416ce91c42530d81e7e5e638f8a5ba
grayolaRSA <gtndanga@gmail.com>
2023-05-17 19:26:11 +0200

    update

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- script creates a database
+-- script creates a new user
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+SET PASSWORD FOR 'user_0d_2'@'localhost' = 'user_0d_2_pwd';
