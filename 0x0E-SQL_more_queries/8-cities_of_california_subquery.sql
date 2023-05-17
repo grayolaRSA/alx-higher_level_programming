c60f1949ffeaa866cca9973fb89f0049c584bf16
grayolaRSA <gtndanga@gmail.com>
2023-05-17 20:38:30 +0200

    update

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,4 @@
 -- script lists cities that can be found in database
--- joins two tables
--- Script that lists all the cities of California registered in the database
 SELECT id, name
 FROM cities
 WHERE state_id = (
