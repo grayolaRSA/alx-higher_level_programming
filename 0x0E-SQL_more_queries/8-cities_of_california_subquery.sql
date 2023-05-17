7dceda06b9fb21317799037866ea4b4a4e00c53d
grayolaRSA <gtndanga@gmail.com>
2023-05-17 20:37:23 +0200

    update

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,9 @@
 -- script lists cities that can be found in database
 -- joins two tables
-USE hbtn_0d_usa;
-SELECT cities.name, states.name
-FROM cities, states
-WHERE states.name = "California"
-AND cities.state_id = states.id
-ORDER BY cities.id;
+-- Script that lists all the cities of California registered in the database
+SELECT id, name
+FROM cities
+WHERE state_id = (
+      SELECT id
+      FROM states
+      WHERE name = "California");
