2d4035c794ee5b07a504fb656e307b7276dc2f58
grayolaRSA <gtndanga@gmail.com>
2023-05-17 20:15:03 +0200

    update

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,7 +2,7 @@
 -- also creates table in database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
 id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
 state_id INT NOT NULL FOREIGN KEY REFERENCES states.id,
 name VARCHAR(256) NOT NULL);
