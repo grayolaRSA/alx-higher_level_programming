c88a209fabb66089ed76561ddb878e0f4f2a32b8
grayolaRSA <gtndanga@gmail.com>
2023-05-17 21:42:43 +0200

    update

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,2 @@
+-- script updates a record
+UPDATE second_table SET score = 10 WHERE name = "Bob";
---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,2 @@
+-- script computes average score of records
+SELECT AVG(score) as average FROM second_table;
