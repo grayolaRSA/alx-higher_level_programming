30c65ef73a56fa99662c7d156f7b949c001b65a6
grayolaRSA <gtndanga@gmail.com>
2023-06-13 21:17:19 +0200

    update

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const SquareN = require('./5-square');
+
+class Square extends SquareN {
+    charPrint (c) {
+      if (c === undefined) {
+	  c = 'X';
+      }
+	for (let i = 0; i < this.width; i++) {
+            let s = '';
+            for (let j = 0; j < this.height; j++) {
+		s += c;
+	    }
+            console.log(s);
+	}
+    }
+}
+module.exports = Square;
