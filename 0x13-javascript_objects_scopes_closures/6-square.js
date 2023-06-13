23121d02e693af2b46856bec4d487a4417f92d95
grayolaRSA <gtndanga@gmail.com>
2023-06-13 21:31:11 +0200

    square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,17 +2,17 @@
 const SquareN = require('./5-square');
 
 class Square extends SquareN {
-    charPrint (c) {
-      if (c === undefined) {
-	  c = 'X';
-      }
-	for (let i = 0; i < this.width; i++) {
-            let s = '';
-            for (let j = 0; j < this.height; j++) {
-		s += c;
-	    }
-            console.log(s);
-	}
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
     }
+    for (let i = 0; i < this.width; i++) {
+      let s = '';
+      for (let j = 0; j < this.height; j++) {
+        s += c;
+      }
+      console.log(s);
+    }
+  }
 }
 module.exports = Square;
