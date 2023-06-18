f524dbc3302a56971963d910bee87f896117e111
grayolaRSA <gtndanga@gmail.com>
2023-06-18 19:54:48 +0200

    update

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -6,7 +6,7 @@ import sys
 from model_state import Base, State
 from model_city import City
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == '__main__':
