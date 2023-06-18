e1bb6d46ac17b7d77d9173fc81ab303a27e616b6
grayolaRSA <gtndanga@gmail.com>
2023-06-18 20:56:47 +0200

    update

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -13,12 +13,10 @@ class State(Base):
     """
     Class defining attributes of each state
     """
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
 
     if __name__ == '__main__':
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                               format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                               pool_pre_ping=True)
-
-        __tablename__ = 'states'
-        id = Column(Integer, unique=True, nullable=False, primary_key=True)
-        name = Column(String(128), nullable=False)
+        db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
