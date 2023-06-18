3a5ccf88ee065e175fc2a693790fd05ad12390b0
grayolaRSA <gtndanga@gmail.com>
2023-06-18 19:34:26 +0200

    update

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -5,7 +5,7 @@ Add new state to lists of states
 import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == '__main__':
