c12a9a48a767add0dc895c700c2f11acb849d01c
grayolaRSA <gtndanga@gmail.com>
2023-07-09 19:38:12 +0200

    update

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
 """
-python script that sends URL request with letter as parameter
+Python script that sends a POST request to the URL and
+to an URL with the letter as a parameter
 """
 import requests
 import sys
 
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        dic = sys.argv[1]
-    else:
-        dic = ""
+    data = {'q': ""}
 
-        payl = {'q': dic}
+    try:
+        data['q'] = sys.argv[1]
+    except:
+        pass
 
-        r = requests.post('http://0.0.0.0:5000/search_user', data=payl)
+    r = requests.post('http://0.0.0.0:5000/search_user', data)
 
-        try:
-            data = r.json()
-            if not json_o:
-                print("No result")
-            else:
-                print("[{}] {}".format(json_o.get('id'), json_o.get('name')))
-        except ValueError:
-            print("Not a valid JSON")
+    try:
+        json_o = r.json()
+        if not json_o:
+            print("No result")
+        else:
+            print("[{}] {}".format(json_o.get('id'), json_o.get('name')))
+    except:
+        print("Not a valid JSON")
