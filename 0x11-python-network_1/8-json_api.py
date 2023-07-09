cf57dc273b82e14e2ef1bceb41fab7d0c51a2cf1
grayolaRSA <gtndanga@gmail.com>
2023-07-09 19:17:45 +0200

    update

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,19 +8,16 @@ import sys
 
 if __name__ == "__main__":
     data = {'q': ""}
-
-    try:
-        data['q'] = sys.argv[1]
-    except ValueError:
-        pass
-
     r = requests.post('http://0.0.0.0:5000/search_user', data)
-
-    try:
-        json_o = r.json()
-        if not json_o:
-            print("No result")
+    if data['q'] != "":
+        try:
+            data['q'] = sys.argv[1]
+            json_o = r.json()
+            if not json_o:
+                print("No result")
+            else:
+                print("[{}] {}".format(json_o.get('id'), json_o.get('name')))
+        except TypeError:
+            print("Not a valid JSON")
         else:
-            print("[{}] {}".format(json_o.get('id'), json_o.get('name')))
-    except TypeError:
-        print("Not a valid JSON")
+            exit(1)
